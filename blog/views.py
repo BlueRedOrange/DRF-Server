03bcf5d0f777012332816028b2f51f67eb6b90a4
BlueRedOrange <jmg0916789@gmail.com>
2024-05-31 20:25:05 +0900

    setting

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@ from rest_framework import generics, mixins
 from rest_framework import viewsets
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 # class PostListCreateAPIView(APIView):
@@ -107,3 +108,14 @@ class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset().order_by('-updated_at'))
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ from datetime import timedelta
 from dotenv import load_dotenv
 import os
 
-AUTH_USER_MODEL = 'accounts.User'
+#AUTH_USER_MODEL = 'accounts.User'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&m9_j+=0*c6ugjbof^o#io4x)+n!6ju68tak#)dey10f^fj4y)'
-
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
